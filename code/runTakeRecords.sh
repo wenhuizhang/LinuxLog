260ec02ca3a75e2127581542016f0d6d2900590d
Wenhui Zhang <wenhui@gwmail.gwu.edu>
2017-06-08 16:00:57 -0400

    Create runTakeRecords.sh

--- code/runTakeRecords.sh
+++ code/runTakeRecords.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # bash script for record taking of registry files
+# for seattle fedora jun 8th 2017
 
 timestamp=$(date +"%T")
 echo $timestamp
@@ -11,12 +12,16 @@ cd ./"$timestamp"
 cp /var/log/audit/audit.log ./
 cp /etc/passwd ./
 cp /etc/shadow ./
-cp /var/log/auth.log ./
-cp /var/log/sudo.log ./
+#cp /var/log/auth.log ./
+#cp /var/log/sudo.log ./
 cp /var/log/wtmp ./
-cp /etc/sysconfig.conf ./
+cp -r /etc/sysconfig.conf ./
 cp -r /tmp ./
-cp /var/log/installer/syslog ./
-cp -r /etc/security  ./	  	  
+#cp /var/log/installer/syslog ./
+cp -r /etc/security  ./	 
+
+cp /var/log/dnf.log ./
+cp /var/log/secure ./
+cp /proc/kmsg ./
 cd ..
 
